f70589cb62f0e518c6d95a1946194cb616fc192b
get.dipen@gmail.com <get.dipen@gmail.com>
2023-02-28 10:54:06 -0800

    added seed data to artists and tracks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create 8 artists
+
+8.times do
+  Artist.create(name: Faker::Music.band, spotify_id: SecureRandom.hex)
+end
+
+# Create 8 tracks
+8.times do
+  Track.create(
+    album: Faker::Music.album,
+    album_image_url: "https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    spotify_id: SecureRandom.hex,
+    name: Faker::Music::RockBand.song,
+    preview_url: "",
+    youtube_url: "",
+    youtube_likes_count: rand(100000),
+    youtube_views_count: rand(1000000),
+    rank: -1,
+    age_top10: rand(1..25)
+  )
+end
+
+# Add upto 3 artists to each track
+Track.all.each do |track|
+  rand(3).times do
+    random_artist = Artist.offset(rand(Artist.count)).first
+    # check if track has random_artist, if not add it
+    track.artists << Artist.offset(rand(Artist.count)).first unless track.artist_ids.include?(random_artist.id)
+  end
+end
---
+++ db/seeds2.rb
@@ -0,0 +1,533 @@
+null = nil
+api_results = {
+  "href": "https://api.spotify.com/v1/playlists/6UeSakyzhiEt4NB3UAd6NQ/tracks?offset=0&limit=100&market=US&locale=en-US,en;q=0.9",
+  "items": [
+    {
+      "added_at": "2023-02-23T22:13:22Z",
+      "added_by": {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/user/billboard.com"
+        },
+        "href": "https://api.spotify.com/v1/users/billboard.com",
+        "id": "billboard.com",
+        "type": "user",
+        "uri": "spotify:user:billboard.com"
+      },
+      "is_local": false,
+      "primary_color": null,
+      "track": {
+        "album": {
+          "album_type": "single",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/5YGY8feqx7naU7z4HrwZM6"
+              },
+              "href": "https://api.spotify.com/v1/artists/5YGY8feqx7naU7z4HrwZM6",
+              "id": "5YGY8feqx7naU7z4HrwZM6",
+              "name": "Miley Cyrus",
+              "type": "artist",
+              "uri": "spotify:artist:5YGY8feqx7naU7z4HrwZM6"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/7I0tjwFtxUwBC1vgyeMAax"
+          },
+          "href": "https://api.spotify.com/v1/albums/7I0tjwFtxUwBC1vgyeMAax",
+          "id": "7I0tjwFtxUwBC1vgyeMAax",
+          "images": [
+            {
+              "height": 640,
+              "url": "https://i.scdn.co/image/ab67616d0000b273f429549123dbe8552764ba1d",
+              "width": 640
+            },
+            {
+              "height": 300,
+              "url": "https://i.scdn.co/image/ab67616d00001e02f429549123dbe8552764ba1d",
+              "width": 300
+            },
+            {
+              "height": 64,
+              "url": "https://i.scdn.co/image/ab67616d00004851f429549123dbe8552764ba1d",
+              "width": 64
+            }
+          ],
+          "name": "Flowers",
+          "release_date": "2023-01-13",
+          "release_date_precision": "day",
+          "total_tracks": 1,
+          "type": "album",
+          "uri": "spotify:album:7I0tjwFtxUwBC1vgyeMAax"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/5YGY8feqx7naU7z4HrwZM6"
+            },
+            "href": "https://api.spotify.com/v1/artists/5YGY8feqx7naU7z4HrwZM6",
+            "id": "5YGY8feqx7naU7z4HrwZM6",
+            "name": "Miley Cyrus",
+            "type": "artist",
+            "uri": "spotify:artist:5YGY8feqx7naU7z4HrwZM6"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 200454,
+        "episode": false,
+        "explicit": false,
+        "external_ids": {
+          "isrc": "USSM12209777"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/0yLdNVWF3Srea0uzk55zFn"
+        },
+        "href": "https://api.spotify.com/v1/tracks/0yLdNVWF3Srea0uzk55zFn",
+        "id": "0yLdNVWF3Srea0uzk55zFn",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Flowers",
+        "popularity": 100,
+        "preview_url": "https://p.scdn.co/mp3-preview/f2857681239dee871da7055d2f725ea6dcd1b4d8?cid=774b29d4f13844c495f206cafdad9c86",
+        "track": true,
+        "track_number": 1,
+        "type": "track",
+        "uri": "spotify:track:0yLdNVWF3Srea0uzk55zFn"
+      },
+      "video_thumbnail": {
+        "url": null
+      }
+    },
+    {
+      "added_at": "2023-02-23T22:13:22Z",
+      "added_by": {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/user/billboard.com"
+        },
+        "href": "https://api.spotify.com/v1/users/billboard.com",
+        "id": "billboard.com",
+        "type": "user",
+        "uri": "spotify:user:billboard.com"
+      },
+      "is_local": false,
+      "primary_color": null,
+      "track": {
+        "album": {
+          "album_type": "album",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/7tYKF4w9nC0nq9CsPZTHyP"
+              },
+              "href": "https://api.spotify.com/v1/artists/7tYKF4w9nC0nq9CsPZTHyP",
+              "id": "7tYKF4w9nC0nq9CsPZTHyP",
+              "name": "SZA",
+              "type": "artist",
+              "uri": "spotify:artist:7tYKF4w9nC0nq9CsPZTHyP"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/07w0rG5TETcyihsEIZR3qG"
+          },
+          "href": "https://api.spotify.com/v1/albums/07w0rG5TETcyihsEIZR3qG",
+          "id": "07w0rG5TETcyihsEIZR3qG",
+          "images": [
+            {
+              "height": 640,
+              "url": "https://i.scdn.co/image/ab67616d0000b27370dbc9f47669d120ad874ec1",
+              "width": 640
+            },
+            {
+              "height": 300,
+              "url": "https://i.scdn.co/image/ab67616d00001e0270dbc9f47669d120ad874ec1",
+              "width": 300
+            },
+            {
+              "height": 64,
+              "url": "https://i.scdn.co/image/ab67616d0000485170dbc9f47669d120ad874ec1",
+              "width": 64
+            }
+          ],
+          "name": "SOS",
+          "release_date": "2022-12-09",
+          "release_date_precision": "day",
+          "total_tracks": 23,
+          "type": "album",
+          "uri": "spotify:album:07w0rG5TETcyihsEIZR3qG"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/7tYKF4w9nC0nq9CsPZTHyP"
+            },
+            "href": "https://api.spotify.com/v1/artists/7tYKF4w9nC0nq9CsPZTHyP",
+            "id": "7tYKF4w9nC0nq9CsPZTHyP",
+            "name": "SZA",
+            "type": "artist",
+            "uri": "spotify:artist:7tYKF4w9nC0nq9CsPZTHyP"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 153946,
+        "episode": false,
+        "explicit": false,
+        "external_ids": {
+          "isrc": "USRC12204584"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/3OHfY25tqY28d16oZczHc8"
+        },
+        "href": "https://api.spotify.com/v1/tracks/3OHfY25tqY28d16oZczHc8",
+        "id": "3OHfY25tqY28d16oZczHc8",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Kill Bill",
+        "popularity": 92,
+        "preview_url": "https://p.scdn.co/mp3-preview/ac3426aecc4cc1fc7c3961aee6e6389c804f01b8?cid=774b29d4f13844c495f206cafdad9c86",
+        "track": true,
+        "track_number": 2,
+        "type": "track",
+        "uri": "spotify:track:3OHfY25tqY28d16oZczHc8"
+      },
+      "video_thumbnail": {
+        "url": null
+      }
+    },
+    {
+      "added_at": "2023-02-23T22:13:22Z",
+      "added_by": {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/user/billboard.com"
+        },
+        "href": "https://api.spotify.com/v1/users/billboard.com",
+        "id": "billboard.com",
+        "type": "user",
+        "uri": "spotify:user:billboard.com"
+      },
+      "is_local": false,
+      "primary_color": null,
+      "track": {
+        "album": {
+          "album_type": "album",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/0iEtIxbK0KxaSlF7G42ZOp"
+              },
+              "href": "https://api.spotify.com/v1/artists/0iEtIxbK0KxaSlF7G42ZOp",
+              "id": "0iEtIxbK0KxaSlF7G42ZOp",
+              "name": "Metro Boomin",
+              "type": "artist",
+              "uri": "spotify:artist:0iEtIxbK0KxaSlF7G42ZOp"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/7txGsnDSqVMoRl6RQ9XyZP"
+          },
+          "href": "https://api.spotify.com/v1/albums/7txGsnDSqVMoRl6RQ9XyZP",
+          "id": "7txGsnDSqVMoRl6RQ9XyZP",
+          "images": [
+            {
+              "height": 640,
+              "url": "https://i.scdn.co/image/ab67616d0000b27313e54d6687e65678d60466c2",
+              "width": 640
+            },
+            {
+              "height": 300,
+              "url": "https://i.scdn.co/image/ab67616d00001e0213e54d6687e65678d60466c2",
+              "width": 300
+            },
+            {
+              "height": 64,
+              "url": "https://i.scdn.co/image/ab67616d0000485113e54d6687e65678d60466c2",
+              "width": 64
+            }
+          ],
+          "name": "HEROES & VILLAINS",
+          "release_date": "2022-12-02",
+          "release_date_precision": "day",
+          "total_tracks": 15,
+          "type": "album",
+          "uri": "spotify:album:7txGsnDSqVMoRl6RQ9XyZP"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/0iEtIxbK0KxaSlF7G42ZOp"
+            },
+            "href": "https://api.spotify.com/v1/artists/0iEtIxbK0KxaSlF7G42ZOp",
+            "id": "0iEtIxbK0KxaSlF7G42ZOp",
+            "name": "Metro Boomin",
+            "type": "artist",
+            "uri": "spotify:artist:0iEtIxbK0KxaSlF7G42ZOp"
+          },
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ"
+            },
+            "href": "https://api.spotify.com/v1/artists/1Xyo4u8uXC1ZmMpatF05PJ",
+            "id": "1Xyo4u8uXC1ZmMpatF05PJ",
+            "name": "The Weeknd",
+            "type": "artist",
+            "uri": "spotify:artist:1Xyo4u8uXC1ZmMpatF05PJ"
+          },
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/1URnnhqYAYcrqrcwql10ft"
+            },
+            "href": "https://api.spotify.com/v1/artists/1URnnhqYAYcrqrcwql10ft",
+            "id": "1URnnhqYAYcrqrcwql10ft",
+            "name": "21 Savage",
+            "type": "artist",
+            "uri": "spotify:artist:1URnnhqYAYcrqrcwql10ft"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 221520,
+        "episode": false,
+        "explicit": true,
+        "external_ids": {
+          "isrc": "USUG12208791"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/2dHHgzDwk4BJdRwy9uXhTO"
+        },
+        "href": "https://api.spotify.com/v1/tracks/2dHHgzDwk4BJdRwy9uXhTO",
+        "id": "2dHHgzDwk4BJdRwy9uXhTO",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Creepin' (with The Weeknd & 21 Savage)",
+        "popularity": 94,
+        "preview_url": null,
+        "track": true,
+        "track_number": 10,
+        "type": "track",
+        "uri": "spotify:track:2dHHgzDwk4BJdRwy9uXhTO"
+      },
+      "video_thumbnail": {
+        "url": null
+      }
+    },
+    {
+      "added_at": "2023-02-23T22:13:22Z",
+      "added_by": {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/user/billboard.com"
+        },
+        "href": "https://api.spotify.com/v1/users/billboard.com",
+        "id": "billboard.com",
+        "type": "user",
+        "uri": "spotify:user:billboard.com"
+      },
+      "is_local": false,
+      "primary_color": null,
+      "track": {
+        "album": {
+          "album_type": "single",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/78rUTD7y6Cy67W1RVzYs7t"
+              },
+              "href": "https://api.spotify.com/v1/artists/78rUTD7y6Cy67W1RVzYs7t",
+              "id": "78rUTD7y6Cy67W1RVzYs7t",
+              "name": "PinkPantheress",
+              "type": "artist",
+              "uri": "spotify:artist:78rUTD7y6Cy67W1RVzYs7t"
+            },
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/3LZZPxNDGDFVSIPqf4JuEf"
+              },
+              "href": "https://api.spotify.com/v1/artists/3LZZPxNDGDFVSIPqf4JuEf",
+              "id": "3LZZPxNDGDFVSIPqf4JuEf",
+              "name": "Ice Spice",
+              "type": "artist",
+              "uri": "spotify:artist:3LZZPxNDGDFVSIPqf4JuEf"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/6cVfHBcp3AdpYY0bBglkLN"
+          },
+          "href": "https://api.spotify.com/v1/albums/6cVfHBcp3AdpYY0bBglkLN",
+          "id": "6cVfHBcp3AdpYY0bBglkLN",
+          "images": [
+            {
+              "height": 640,
+              "url": "https://i.scdn.co/image/ab67616d0000b27342c5ba689b2e7cbc208a8fa7",
+              "width": 640
+            },
+            {
+              "height": 300,
+              "url": "https://i.scdn.co/image/ab67616d00001e0242c5ba689b2e7cbc208a8fa7",
+              "width": 300
+            },
+            {
+              "height": 64,
+              "url": "https://i.scdn.co/image/ab67616d0000485142c5ba689b2e7cbc208a8fa7",
+              "width": 64
+            }
+          ],
+          "name": "Boy's a liar Pt. 2",
+          "release_date": "2023-02-03",
+          "release_date_precision": "day",
+          "total_tracks": 2,
+          "type": "album",
+          "uri": "spotify:album:6cVfHBcp3AdpYY0bBglkLN"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/78rUTD7y6Cy67W1RVzYs7t"
+            },
+            "href": "https://api.spotify.com/v1/artists/78rUTD7y6Cy67W1RVzYs7t",
+            "id": "78rUTD7y6Cy67W1RVzYs7t",
+            "name": "PinkPantheress",
+            "type": "artist",
+            "uri": "spotify:artist:78rUTD7y6Cy67W1RVzYs7t"
+          },
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/3LZZPxNDGDFVSIPqf4JuEf"
+            },
+            "href": "https://api.spotify.com/v1/artists/3LZZPxNDGDFVSIPqf4JuEf",
+            "id": "3LZZPxNDGDFVSIPqf4JuEf",
+            "name": "Ice Spice",
+            "type": "artist",
+            "uri": "spotify:artist:3LZZPxNDGDFVSIPqf4JuEf"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 131013,
+        "episode": false,
+        "explicit": false,
+        "external_ids": {
+          "isrc": "GBAYE2300015"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/6AQbmUe0Qwf5PZnt4HmTXv"
+        },
+        "href": "https://api.spotify.com/v1/tracks/6AQbmUe0Qwf5PZnt4HmTXv",
+        "id": "6AQbmUe0Qwf5PZnt4HmTXv",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Boy's a liar Pt. 2",
+        "popularity": 91,
+        "preview_url": "https://p.scdn.co/mp3-preview/eb1571e89783c04e4ac63247be85fd059365151e?cid=774b29d4f13844c495f206cafdad9c86",
+        "track": true,
+        "track_number": 1,
+        "type": "track",
+        "uri": "spotify:track:6AQbmUe0Qwf5PZnt4HmTXv"
+      },
+      "video_thumbnail": {
+        "url": null
+      }
+    },
+    {
+      "added_at": "2023-02-23T22:13:22Z",
+      "added_by": {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/user/billboard.com"
+        },
+        "href": "https://api.spotify.com/v1/users/billboard.com",
+        "id": "billboard.com",
+        "type": "user",
+        "uri": "spotify:user:billboard.com"
+      },
+      "is_local": false,
+      "primary_color": null,
+      "track": {
+        "album": {
+          "album_type": "single",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/4oUHIQIBe0LHzYfvXNW4QM"
+              },
+              "href": "https://api.spotify.com/v1/artists/4oUHIQIBe0LHzYfvXNW4QM",
+              "id": "4oUHIQIBe0LHzYfvXNW4QM",
+              "name": "Morgan Wallen",
+              "type": "artist",
+              "uri": "spotify:artist:4oUHIQIBe0LHzYfvXNW4QM"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/7fOmdhRrRohTzToL617xkk"
+          },
+          "href": "https://api.spotify.com/v1/albums/7fOmdhRrRohTzToL617xkk",
+          "id": "7fOmdhRrRohTzToL617xkk",
+          "images": [
+            {
+              "height": 640,
+              "url": "https://i.scdn.co/image/ab67616d0000b273fc1df8423733f6f3c9e8dea2",
+              "width": 640
+            },
+            {
+              "height": 300,
+              "url": "https://i.scdn.co/image/ab67616d00001e02fc1df8423733f6f3c9e8dea2",
+              "width": 300
+            },
+            {
+              "height": 64,
+              "url": "https://i.scdn.co/image/ab67616d00004851fc1df8423733f6f3c9e8dea2",
+              "width": 64
+            }
+          ],
+          "name": "3 Songs At A Time Sampler",
+          "release_date": "2023-01-31",
+          "release_date_precision": "day",
+          "total_tracks": 3,
+          "type": "album",
+          "uri": "spotify:album:7fOmdhRrRohTzToL617xkk"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/4oUHIQIBe0LHzYfvXNW4QM"
+            },
+            "href": "https://api.spotify.com/v1/artists/4oUHIQIBe0LHzYfvXNW4QM",
+            "id": "4oUHIQIBe0LHzYfvXNW4QM",
+            "name": "Morgan Wallen",
+            "type": "artist",
+            "uri": "spotify:artist:4oUHIQIBe0LHzYfvXNW4QM"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 163854,
+        "episode": false,
+        "explicit": true,
+        "external_ids": {
+          "isrc": "USUG12300802"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/59uQI0PADDKeE6UZDTJEe8"
+        },
+        "href": "https://api.spotify.com/v1/tracks/59uQI0PADDKeE6UZDTJEe8",
+        "id": "59uQI0PADDKeE6UZDTJEe8",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Last Night",
+        "popularity": 86,
+        "preview_url": null,
+        "track": true,
+        "track_number": 1,
+        "type": "track",
+        "uri": "spotify:track:59uQI0PADDKeE6UZDTJEe8"
+      },
+      "video_thumbnail": {
+        "url": null
+      }
+    }
+  ],
+  "limit": 100,
+  "next": null,
+  "offset": 0,
+  "previous": null,
+  "total": 100
+}
+
+album = api_results[:items][0][:track][:album][:name]
+artist0 = api_results[:items][0][:track][:artists][0][:name]
+name = api_results[:items][0][:track][:name]
+
+puts artist0, name
+puts album
